e737048191ae8b92a36873b894324454d2a39a1d
jenscancio <jens.cancio@epfl.ch>
2026-01-27 09:50:54 +0100

    feat: fix Globals.PARAMETERS initialization and implement LIC 2 unit tests (closes #7)

--- Interceptor/src/test/java/interceptor/lic/Lic2Test.java
+++ Interceptor/src/test/java/interceptor/lic/Lic2Test.java
@@ -3,36 +3,52 @@ package interceptor.lic;
 import interceptor.model.Globals;
 import interceptor.model.Parameters;
 import interceptor.model.Point;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import static org.junit.jupiter.api.Assertions.*;
 
 public class Lic2Test {
 
+    @BeforeEach
+    public void setup() {
+
+        Globals.PARAMETERS = new Parameters();
+
+
+        Globals.POINTS = new Point[100];
+        Globals.NUMPOINTS = 0;
+    }
+
     @Test
     public void testLic2Positive() {
-        // Setup: A right angle (PI/2). PI/2 is < (PI - 0.5)
         Globals.PARAMETERS.EPSILON = 0.5;
         Globals.NUMPOINTS = 3;
-        Globals.POINTS = new Point[]{
-                new Point(1, 0),
-                new Point(0, 0), // Vertex
-                new Point(0, 1)
-        };
+        Globals.POINTS[0] = new Point(1, 0);
+        Globals.POINTS[1] = new Point(0, 0);
+        Globals.POINTS[2] = new Point(0, 1);
 
         assertTrue(Lic2.evaluate(), "Angle of PI/2 should satisfy condition when EPSILON is 0.5");
     }
 
     @Test
     public void testLic2NegativeUndefined() {
-        // Setup: Point coincides with vertex
         Globals.PARAMETERS.EPSILON = 0.1;
         Globals.NUMPOINTS = 3;
-        Globals.POINTS = new Point[]{
-                new Point(0, 0),
-                new Point(0, 0), // Vertex coincides with P1
-                new Point(0, 1)
-        };
+        Globals.POINTS[0] = new Point(0, 0);
+        Globals.POINTS[1] = new Point(0, 0);
+        Globals.POINTS[2] = new Point(0, 1);
 
         assertFalse(Lic2.evaluate(), "Undefined angle should not satisfy LIC 2");
+    }
+
+    @Test
+    public void testLic2NegativeInRange() {
+        Globals.PARAMETERS.EPSILON = 0.1;
+        Globals.NUMPOINTS = 3;
+        Globals.POINTS[0] = new Point(1, 0);
+        Globals.POINTS[1] = new Point(0, 0);
+        Globals.POINTS[2] = new Point(-1, 0);
+
+        assertFalse(Lic2.evaluate(), "A straight line should not satisfy LIC 2");
     }
 }
