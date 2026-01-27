1300fc9b77018f0b24f08976bf0c8aa109e7c7e6
Anna Remmare <remmare@kth.se>
2026-01-27 10:15:25 +0100

    ADD: PUM and corresponding test #21

--- Interceptor/src/main/java/interceptor/unlock/Pum.java
+++ Interceptor/src/main/java/interceptor/unlock/Pum.java
@@ -1,5 +1,32 @@
 package interceptor.unlock;
+import interceptor.model.Connector;
 
 public class Pum {
-    
+    private boolean[][] pum;
+
+    public Pum(boolean[] cmv, Connector[][] lcm) {
+        int size = cmv.length;
+        pum = new boolean[size][size];
+
+        // Form the Preliminary Unlocking Matrix (PUM) using CMV and LCM as specified
+        for (int i = 0; i < size; i++) {
+            for (int j = 0; j < size; j++) {
+
+                if (lcm[i][j] == Connector.NOTUSED) {
+                    // If LCM[i,j] is NOTUSED, then PUM[i,j] is set to true
+                    pum[i][j] = true;
+                } else if (lcm[i][j] == Connector.ANDD) {
+                    // If LCM[i,j] is ANDD, PUM[i,j] is true only if CMV[i] AND CMV[j] is true
+                    pum[i][j] = cmv[i] && cmv[j];
+                } else { // ORR
+                    // If LCM[i,j] is ORR, PUM[i,j] is true if CMV[i] OR CMV[j] is true
+                    pum[i][j] = cmv[i] || cmv[j];
+                }
+            }
+        }
+    }
+
+    public boolean get(int i, int j) {
+        return pum[i][j];
+    }
 }
