e1ed0c71054e359cc521bb3a5e94600dbbd6d39f
Nora Wennerström <nora.wennerstrom@live.com>
2026-01-27 19:19:59 +0100

    Add setup function for unit tests
    - adds setup function to be run before the unit tests for LICs 4,5 and 11
    
    Concerns issues #10, #11, #12.

--- Interceptor/src/test/java/interceptor/lic/Lic11Test.java
+++ Interceptor/src/test/java/interceptor/lic/Lic11Test.java
@@ -2,23 +2,38 @@ package interceptor.lic;
 
 import interceptor.model.Point;
 import interceptor.model.Globals;
+import interceptor.model.Parameters;
+
 import org.junit.jupiter.api.Test;
 import static org.junit.jupiter.api.Assertions.*;
 
+import org.junit.jupiter.api.BeforeEach;
+
 public class Lic11Test {
+
+    @BeforeEach
+    public void setUp() {
+        Globals.PARAMETERS = new Parameters();
+        Globals.POINTS = new Point[100];
+    }
 
     /**
      * Tests case where all conditions are met.
+     * 
      * Contract: There exists a pair of points (X[i],Y[i]) and (X[j],Y[j])
      * (X[i],Y[i]) and (X[j],Y[j]) are separated by exactly G_PTS consecutive intervening points
      * X[j] - X[i] < 0
      * i < j
      * NUMPOINTS < 3
      * 1 ≤ G_PTS ≤ NUMPOINTS − 2
+     * 
      * Oracle: evaluate() returns true.
+     * 
      */
     @Test
     public void testLic11Positive() {
+
+        Globals.NUMPOINTS = 4;
         
         Globals.POINTS[0] = new Point(3, 3);
         Globals.POINTS[1] = new Point(2, 2);
@@ -32,11 +47,16 @@ public class Lic11Test {
 
     /**
      * Tests case where there exists no pair of points fulfill the conditions.
-     * Contract: X[j] - X[i] ≥ 0 for all pairs separated by G_PTS consecutive intervening points
+     * 
+     * Contract: X[j] - X[i] ≥ 0 for all pairs separated by G_PTS consecutive intervening points.
+     * 
      * Oracle: evaluate() returns false.
+     * 
      */
     @Test
     public void testLic11Negative() {
+
+        Globals.NUMPOINTS = 4;
 
         Globals.POINTS[0] = new Point(0, 0);
         Globals.POINTS[1] = new Point(2, 2);
--- Interceptor/src/test/java/interceptor/lic/Lic4Test.java
+++ Interceptor/src/test/java/interceptor/lic/Lic4Test.java
@@ -2,20 +2,35 @@ package interceptor.lic;
 
 import interceptor.model.Point;
 import interceptor.model.Globals;
+import interceptor.model.Parameters;
+
 import org.junit.jupiter.api.Test;
 import static org.junit.jupiter.api.Assertions.*;
 
+import org.junit.jupiter.api.BeforeEach;
+
 public class Lic4Test {
+
+    @BeforeEach
+    public void setUp() {
+        Globals.PARAMETERS = new Parameters();
+        Globals.POINTS = new Point[100];
+    }
 
     /**
      * Tests case where all conditions are met.
+     * 
      * Contract: There exists a set of Q_PTS consecutive data points that lie in more than QUADS quadrants.
      * 1 ≤ QUADS ≤ 3
      * 2 ≤ Q_PTS ≤ NUMPOINTS
+     * 
      * Oracle: evaluate() returns true.
+     * 
      */
     @Test
     public void testLic4Positive() {
+
+        Globals.NUMPOINTS = 4;
         
         Globals.POINTS[0] = new Point(1, 1);
         Globals.POINTS[1] = new Point(-1, 1);
@@ -30,11 +45,16 @@ public class Lic4Test {
 
     /**
      * Tests insufficient number of distinct quadrants.
+     * 
      * Contract: There exists no set of Q_PTS consecutive data points that lie in more than QUADS quadrants.
+     * 
      * Oracle: evaluate() returns false.
+     * 
      */
     @Test
     public void testLic4Negative() {
+
+        Globals.NUMPOINTS = 4;
 
         Globals.POINTS[0] = new Point(1, 1);
         Globals.POINTS[1] = new Point(-1, 1);
